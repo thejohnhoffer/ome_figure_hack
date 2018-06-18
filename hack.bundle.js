9946dcffec860c9940f707ac7321f6dcff31a364
John Hoffer <john@hoff.in>
2018-06-18 15:17:07 -0400

    uglify

--- hack.bundle.js
+++ hack.bundle.js
@@ -1,12 +1,17 @@
-(function() {
+"use strict";
+
+(function () {
   var MODELS = window.figureModel.panels.models;
-  var MODEL = MODELS.filter(m=>m.attributes.selected)[0]; 
-  var LABELS = MODEL.attributes.channels.map(c => c.label); 
-  var ID = MODEL.attributes.imageId; 
+  var MODEL = MODELS.filter(function (m) {
+    return m.attributes.selected;
+  })[0];
+  var LABELS = MODEL.attributes.channels.map(function (c) {
+    return c.label;
+  });
+  var ID = MODEL.attributes.imageId;
   var URL = "https://omero.hms.harvard.edu/pathviewer/viewer/" + ID + "/channelgroups";
 
-
-  var setSliderContent = (content, i) => {
+  var setSliderContent = function setSliderContent(content, i) {
     var root = 'channel_sliders';
     var sliders = document.getElementById(root).firstChild;
     var output = document.createElement("span");
@@ -21,41 +26,39 @@
     try {
       var group = sliders.children[i].getElementsByClassName('ch_slider')[0];
       group.appendChild(output);
-    }
-    catch (e) {
+    } catch (e) {
       console.error('No button ' + i + ' for ' + content);
     }
-  }
-  var readGroup = (list, group) => {
-    return list.concat(group.channels.map((channel, i) => {
+  };
+  var readGroup = function readGroup(list, group) {
+    return list.concat(group.channels.map(function (channel, i) {
       var label = LABELS[channel];
       if (i == 0) {
         return group.name + ": " + label;
       }
       return "" + label;
     }));
-  }
+  };
 
-  var readJSON = response => {
+  var readJSON = function readJSON(response) {
     var groups = JSON.parse(response).groups;
     var config_list = groups.reduce(readGroup, []);
     config_list.forEach(setSliderContent);
-    document.body.addEventListener('mouseup', ()=>{
-      window.setTimeout(() => {
+    document.body.addEventListener('mouseup', function () {
+      window.setTimeout(function () {
         config_list.forEach(setSliderContent);
       }, 100);
-    }, true); 
-  }
-  var loadJSON = function(url) {
+    }, true);
+  };
+  var loadJSON = function loadJSON(url) {
     var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
       if (this.readyState == 4 && this.status == 200) {
         readJSON(this.responseText);
       }
-    }
+    };
     xhttp.open("GET", url, true);
     xhttp.send();
-  }
+  };
   loadJSON(URL);
 })();
-
